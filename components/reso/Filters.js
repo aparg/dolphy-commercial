3cc2c31907675f50058bee70c5a04b938968ecf9
aparg <apargtm@gmail.com>
2024-04-22 13:26:34 +0545

    style: filter style

--- components/reso/Filters.js
+++ components/reso/Filters.js
@@ -88,7 +88,9 @@ const Filters = ({
     <>
       <div
         className={`filters d-flex gap-2 gap-md-3 my-2 flex bg-white ${
-          navbar ? `filter__scrolled mt-4 pb-2 container-fluid` : `top-[0px]`
+          navbar
+            ? `filter__scrolled mt-4 pb-2 container-fluid`
+            : `top-[0px] h-10 items-center`
         }`}
       >
         <IndividualFilterButton
