888c68792bb6c5a4b8f91ae07fc11ea17347c10e
aparg <apargtm@gmail.com>
2024-10-22 13:53:12 +0545

    feat: searchbar with debounce

--- components/reso/SearchBar.js
+++ components/reso/SearchBar.js
@@ -1,17 +1,48 @@
-// SearchBar.js
 "use client";
-import React from "react";
-import { useState } from "react";
-import Link from "next/link";
-import Autosuggest from "react-autosuggest";
-import { useRouter } from "next/navigation";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { CgSearch } from "react-icons/cg";
+import { searchProperties } from "@/api/searchProperties";
+import debounce from "lodash.debounce";
+import Autosuggest from "./Autosuggest";
+import { Button } from "@nextui-org/react";
+import useDeviceView from "@/helpers/useDeviceView";
 
-const SearchBar = () => {
-  const [value, setValue] = useState("");
+const SearchBar = ({
+  numberOfSuggestions = 10,
+  small = false,
+  placeholder = "Search by address, city, neighbourhood or postal code",
+}) => {
+  const [displaySuggestions, setDisplaySuggestions] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const { isMobileView } = useDeviceView();
+  // Debouncing
+  const handleChange = async (value) => {
+    await getSuggestions(value);
+  };
 
-  const router = useRouter();
+  const debouncedResults = useMemo(() => {
+    return debounce(async (value) => await handleChange(value), 100);
+  }, []);
 
+  useEffect(() => {
+    return () => {
+      debouncedResults.cancel();
+    };
+  }, []);
+
+  const getSuggestions = async (searchTerm) => {
+    const inputValueLowerCase = searchTerm?.trim()?.toLowerCase();
+    const filteredCities = citiesWithProvinces.filter((data) =>
+      data.city.toLowerCase().includes(inputValueLowerCase)
+    );
+    // return filteredCities;
+    const filteredProperties = await searchProperties(searchTerm);
+    // const addressArray = filteredProperties.map((property, idx) => {
+    //   return property.Address;
+    // });
+    setSuggestions([...filteredCities, ...filteredProperties]);
+  };
   const citiesWithProvinces = [
     { city: "Barrie", province: "Ontario" },
     { city: "Belleville", province: "Ontario" },
@@ -65,7 +96,6 @@ const SearchBar = () => {
     { city: "Welland", province: "Ontario" },
     { city: "Windsor", province: "Ontario" },
     { city: "Woodstock", province: "Ontario" },
-    // Add more cities here
     { city: "Ajax", province: "Ontario" },
     { city: "Whitby", province: "Ontario" },
     { city: "Courtice", province: "Ontario" },
@@ -74,102 +104,70 @@ const SearchBar = () => {
     { city: "Bradford", province: "Ontario" },
   ];
 
-  // Function to get suggestions based on user input
-  const getSuggestions = (inputValue) => {
-    const inputValueLowerCase = inputValue.trim().toLowerCase();
-    return citiesWithProvinces.filter((data) =>
-      data.city.toLowerCase().includes(inputValueLowerCase)
-    );
-  };
-
-  // Triggered when the input value changes
-  const onSuggestionsFetchRequested = ({ value }) => {
-    setSuggestions(getSuggestions(value));
-  };
-
-  // Triggered when the input value is cleared
-  const onSuggestionsClearRequested = () => {
-    setSuggestions([]);
-  };
-
-  const prependToLocalStorageArray = (newValue) => {
-    // Retrieve the array from local storage or initialize an empty array
-    let storedArray = JSON.parse(localStorage.getItem("recentSearch")) || [];
-
-    // Prepend the new value to the array
-    if (!storedArray.includes(newValue)) storedArray.unshift(newValue);
-    storedArray = storedArray.slice(0, 3);
-    // Store the updated array back to local storage
-    localStorage.setItem("recentSearch", JSON.stringify(storedArray));
-  };
-
-  // Render Each Option
-  const renderSuggestion = (suggestion) => (
-    <div onClick={() => prependToLocalStorageArray({ city: suggestion.city })}>
-      <Link
-        href={`/${suggestion.province.toLowerCase()}/${suggestion.city.toLowerCase()}`}
-        className="ha-link"
-      >
-        <div className="d-flex justify-content-between me-3">
-          <div className="d-flex gap-1 justify-content-start">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="#00b5d6"
-                className="bi bi-geo"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"
-                />
-              </svg>
-            </div>
-
-            <span className="me-4">{suggestion.city}</span>
-          </div>
-        </div>
-      </Link>
-    </div>
-  );
-
-  // Autosuggest input props
-  const inputProps = {
-    placeholder: "Search for city",
-    // className: "searchbar",
-    value,
-    onChange: (event, { newValue }) => setValue(newValue),
-    onKeyDown: (event) => {
-      // Check if the pressed key is Enter (key code 13)
-      if (event.key === "Enter") {
-        // Get the first suggestion (if available)
-        const firstSuggestion = suggestions[0];
-
-        // If there is a suggestion, navigate to its link
-        if (firstSuggestion) {
-          prependToLocalStorageArray({ city: firstSuggestion.city });
-          router.push(
-            `/${firstSuggestion.province.toLowerCase()}/${firstSuggestion.city.toLowerCase()}`
-          );
-        }
-      }
-    },
-    //additional
-    // highlightFirstSuggestion: true,
-    // alwaysRenderSuggestions: true,
-  };
+  //style for input box
+  let inputBoxClass = [];
+  if (!small && !displaySuggestions) {
+    inputBoxClass.push("border-2 border-black");
+  } else if (!displaySuggestions) {
+    inputBoxClass.push("border-1 border-gray-300");
+  }
+  if (displaySuggestions) {
+    inputBoxClass.push("border-1 border-black rounded-t-[28px]");
+  } else {
+    inputBoxClass.push("rounded-[28px]");
+  }
 
   return (
-    <Autosuggest
-      suggestions={suggestions}
-      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-      onSuggestionsClearRequested={onSuggestionsClearRequested}
-      getSuggestionValue={(suggestion) => suggestion?.city}
-      renderSuggestion={renderSuggestion}
-      inputProps={inputProps}
-    />
+    <div className={`flex flex-col relative rounded-[28px]`}>
+      <div
+        className={`w-full h-full flex overflow-hidden ${inputBoxClass.join(
+          " "
+        )} shadow-none! z-10`}
+      >
+        <input
+          className={`w-full ${
+            small ? "py-1" : "py-3"
+          } px-2 focus:outline-none focus:shadow-2xl text-center placeholder:text-center sm:placeholder:text-medium`}
+          placeholder={displaySuggestions ? "" : placeholder}
+          onChange={(e) => {
+            setDisplaySuggestions(true);
+            setSearchTerm(e.target.value);
+            debouncedResults(e.target.value);
+          }}
+          onFocus={() => {
+            setDisplaySuggestions(true);
+          }}
+          onBlur={() => {
+            setTimeout(() => setDisplaySuggestions(false), 200);
+          }}
+          value={searchTerm}
+        />
+        <div className="flex items-center pr-1 pl-0 justify-center bg-white">
+          {/* <CgSearch size="1.25rem" /> */}
+          {!small && !isMobileView ? (
+            <Button className={`bg-black rounded-full text-white font-medium`}>
+              {" "}
+              Search{" "}
+            </Button>
+          ) : small ? (
+            <CgSearch size="1.25rem" className="mr-2" />
+          ) : (
+            <CgSearch size="1.75rem" className="mr-2" />
+          )}
+        </div>
+      </div>
+      <div className="relative">
+        {displaySuggestions && (
+          <Autosuggest
+            displaySuggestions={displaySuggestions}
+            searchTerm={searchTerm}
+            suggestions={suggestions}
+            numberOfSuggestions={numberOfSuggestions}
+            setSearchTerm={setSearchTerm}
+          />
+        )}
+      </div>
+    </div>
   );
 };
 
